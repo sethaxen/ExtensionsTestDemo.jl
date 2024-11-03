7490382cbe66b674dfa728152ce28edc9b03e675
Seth Axen <seth@sethaxen.com>
2024-11-03 16:20:14 +0100

    Fix extension loading

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,11 @@ using Test
 
 @testset "ExtensionsTestDemo.jl" begin
     @testset "Extension depending on StatsBase and Random is loaded when only StatsBase is loaded" begin
-        @test Base.get_extension(Main, :ExtensionsTestDemoStatsBaseExt) isa Module
+        @test Base.get_extension(ExtensionsTestDemo, :ExtensionsTestDemoStatsBaseExt) isa Module
     end
 
     @testset "Extension depending on StatsBase and Random is loaded when both are loaded" begin
         using Random
-        @test Base.get_extension(Main, :ExtensionsTestDemoStatsBaseExt) isa Module
+        @test Base.get_extension(ExtensionsTestDemo, :ExtensionsTestDemoStatsBaseExt) isa Module
     end
 end
